0ddecdea9bb961ff483168a05286f9edd429f201
GeorgiPopovIT <gogopopo26062003@gmail.com>
2022-06-02 15:02:46 +0300

    Common change

---
+++ Blog.Web/Extensions/AppExtension.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace Blog.Web.Extensions
+{
+    public static class AppExtension
+    {
+        public const string DirectoryPathSaveImage = $"/img/posts/";
+        public static string GetUserId(this ClaimsPrincipal user) => user.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
