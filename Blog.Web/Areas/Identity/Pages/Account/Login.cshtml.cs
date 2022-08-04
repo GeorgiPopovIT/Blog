cf90bd706ab94df49f5f7d1744c5ee6b42ddb0f3
GeorgiPopovIT <gogopopo262003@gmail.com>
2022-08-04 16:07:59 +0300

    Added facebook authentication.

--- Blog.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Blog.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -86,7 +86,7 @@ namespace Blog.Web.Areas.Identity.Pages.Account
             // Clear the existing external cookie to ensure a clean login process
             await HttpContext.SignOutAsync(IdentityConstants.ExternalScheme);
 
-          //  ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             ReturnUrl = returnUrl;
         }
--- Blog.Web/Program.cs
+++ Blog.Web/Program.cs
@@ -89,5 +89,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
+app.UseAuthentication();;
 
 app.Run();
